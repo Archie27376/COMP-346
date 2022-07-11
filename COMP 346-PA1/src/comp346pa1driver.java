35b39dfbf27c0f60f4f8ed1bf26ff93f20197c84
dante-di-domenico <72292738+Archie27376@users.noreply.github.com>
2022-07-11 16:18:07 -0400

    Update comp346pa1driver.java

--- COMP 346-PA1/src/comp346pa1driver.java
+++ COMP 346-PA1/src/comp346pa1driver.java
@@ -12,6 +12,52 @@ import java.io.PrintStream;
  * @author Kerly Titus
  */
 public class comp346pa1driver {
+package comp346pa1s2020;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kerly Titus
+ */
+public class comp546pa1driver {
+
+    /** 
+     * main class
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    	
+    	try{
+            PrintStream output = new PrintStream("Test3.txt");
+            System.setOut(output);
+
+        }catch(FileNotFoundException e){
+            System.out.println("FILE NOT FOUND");
+            System.exit(0);
+        }
+    	
+    	 /*******************************************************************************************************************************************
+    	  * TODO : implement all the operations of main class   																					*
+    	  ******************************************************************************************************************************************/
+        
+    	Network objNetwork = new Network("network");            /* Activate the network */
+        objNetwork.start();
+        Server objServer = new Server();                        /* Start the server */ 
+        objServer.start();
+        Client objClient1 = new Client("sending");              /* Start the sending client */
+        objClient1.start();
+        Client objClient2 = new Client("receiving");            /* Start the receiving client */
+        objClient2.start();
+    }
+}
 
     /** 
      * main class
